a51392f30d4a6093382cf275ccc346e6ad108227
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-09-10 21:17:38 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -35,7 +35,7 @@ VIDEOS_DICT = {
 
 # ML Model config
 MODEL_DIR = ROOT / 'weights'
-DETECTION_MODEL =  'yolov10s.pt'
+DETECTION_MODEL =  MODEL_DIR / 'yolov10s.pt'
 # In case of your custome model comment out the line above and
 # Place your custom model pt file name at the line below 
 # DETECTION_MODEL = MODEL_DIR / 'my_detection_model.pt'
