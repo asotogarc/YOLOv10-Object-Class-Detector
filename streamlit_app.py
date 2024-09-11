992e4174ecfd999d6f4d4d52c606bff78bb12d96
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-09-11 17:47:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ import streamlit as st
 # Local Modules
 import settings
 import helper
-from settings import load_model
 
 # Setting page layout
 st.set_page_config(
@@ -58,7 +57,7 @@ elif model_type == 'Segmentation':
 
 # Load Pre-trained ML Model
 try:
-    model = load_model(model_path)
+    model = helper.load_model(model_path)
 
 except Exception as ex:
     st.error(f"Unable to load model. Check the specified path: {model_path}")
